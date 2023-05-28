6158cc8ae2e389c3dd46c751ad9918666033803d
Александр Канов <monersen@gmail.com>
2023-05-28 13:57:29 +0300

    Сортировки

---
+++ 3.1/3.1.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void print_arr(int* arr, int arr_lenght) {
+	for (int i = 0; i < arr_lenght; ++i) {
+		if (i < arr_lenght - 1) {
+			cout << arr[i] << ", ";
+		}
+		else {
+			cout << arr[i] << '.';
+		}
+
+	}
+}
+
+void merge_sort(int* arr, const int arr_lenght) {
+	if (arr_lenght == 1) {
+		return;
+	}
+	else {
+		int* arr_left = new int[arr_lenght / 2];
+		int* arr_right = new int[arr_lenght - (arr_lenght / 2)];
+		int index_left = 0, index_right = 0, index_arr = 0;
+
+		for (int i = 0; i < arr_lenght; ++i) {
+			if (i < arr_lenght / 2) {
+				arr_left[i] = arr[i];
+			}
+			else {
+				arr_right[i - (arr_lenght / 2)] = arr[i];
+			}
+		}
+
+		while (index_left < (arr_lenght / 2) || index_right < (arr_lenght - (arr_lenght / 2))) {
+			if (index_left == arr_lenght / 2) {
+				arr[index_arr] = arr_right[index_right];
+				++index_right;
+			}
+			else if (index_right == (arr_lenght - (arr_lenght / 2))) {
+				arr[index_arr] = arr_left[index_left];
+				++index_left;
+			}
+			else {
+				if (arr_left[index_left] <= arr_right[index_right]) {
+					arr[index_arr] = arr_left[index_left];
+					++index_left;
+				}
+				else {
+					arr[index_arr] = arr_right[index_right];
+					++index_right;
+				}
+			}
+			++index_arr;
+		}
+		delete[] arr_left, arr_right;
+	}
+	return;
+}
+
+void sort(int* arr, const int arr_lenght) {
+	if (arr_lenght == 1) {
+		return;
+	}
+	else {
+
+	}
+	return;
+}
+
+int main(int argc, char** argv) {
+
+	int arr[18] = { 24, 66, 20, 79, 30, 16, 19, 62, 94, 59, 0, 7, 59, 90, 84, 60, 95, 62 };
+	int arr_lenght = sizeof(arr) / sizeof(arr[0]);
+
+	cout << "Initial array:\t";
+	print_arr(arr, arr_lenght);
+	cout << endl;
+	merge_sort(arr, arr_lenght);
+	cout << "Sorted array:\t";
+	print_arr(arr, arr_lenght);
+
+	return 0;
+}
---
+++ 3.2/3.2.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void printArray(int* array, const size_t arr_lenght) {
+	for (size_t i = 0; i < arr_lenght; ++i) {
+		if (i < arr_lenght - 1) {
+			cout << array[i] << ", ";
+		}
+		else {
+			cout << array[i] << '.';
+		}
+	}
+}
+
+void qsortRecursive(int* array, const size_t arr_lenght) {
+	const int array_start = 0;
+	const int array_end = (arr_lenght - 1);
+
+	int left_index = array_start;
+	int right_index = array_end;
+
+	int pivot_value = array[arr_lenght / 2];
+
+	while (left_index <= right_index) {
+		while (array[left_index] < pivot_value) {
+			++left_index;
+		}
+		while (array[right_index] > pivot_value) {
+			--right_index;
+		}
+		if (left_index <= right_index) {
+			swap(array[left_index], array[right_index]);
+			++left_index;
+			--right_index;
+		}
+	}
+
+	if (right_index > array_start) {
+		qsortRecursive(&array[array_start], (right_index + 1));
+	}
+	if (left_index < arr_lenght) {
+		qsortRecursive(&array[left_index], (arr_lenght - left_index));
+	}
+}
+
+int main(int argc, char** argv) {
+
+	int arr[18] = { 24, 66, 20, 79, 30, 16, 19, 62, 94, 59, 0, 7, 59, 90, 84, 60, 95, 62 };
+	size_t arr_lenght = sizeof(arr) / sizeof(arr[0]);
+
+	cout << "Initial array:\t";
+	printArray(arr, arr_lenght);
+	cout << endl;
+	qsortRecursive(arr, arr_lenght);
+	cout << "Sorted array:\t";
+	printArray(arr, arr_lenght);
+	cout << endl;
+
+	return 0;
+}
